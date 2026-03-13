5aadb48bd283be20033ff89315200c396f25b948
Pooja Bogam <pjalgee@gmail.com>
2026-03-13 10:01:37 +0530

    Add files via upload
    added new file from remote

---
+++ Third.java
@@ -0,0 +1,23 @@
+package day5_array;
+
+public class Third {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {2,5,6,7,8,9,10,11,12,13,14,15,16};
+		
+	//	System.out.println(arr.length);
+		
+		for(int i=0; i<arr.length; i++)
+				{
+			
+			if(arr[i] % 2==0)
+			{
+				System.out.println(arr[i]);
+			}
+		}
+			
+
+	}
+
+}
