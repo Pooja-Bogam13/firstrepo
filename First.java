f3c4bef5477a6295916a34ade1d4c396a06b97cf
Pooja-Bogam13 <pjalgee@gmail.com>
2026-03-13 09:46:23 +0530

    added new file

---
+++ First.java
@@ -0,0 +1,26 @@
+package day5_array;
+
+public class First {
+
+	public static void main(String[] args) {
+		// array declaration
+		
+		int a[] = new int[5];
+		
+		a[0] = 30;
+		a[1] = 33;
+		a[2] = 35;
+		a[3] = 36;
+		a[4] = 37;
+		
+		for (int i=0; i<5; i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+		
+		
+
+	}
+
+}
