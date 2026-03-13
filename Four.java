55e45d9ef640cade927e5696ddb80575044789a0
Pooja Bogam <pjalgee@gmail.com>
2026-03-13 09:57:31 +0530

    new file uploaded from remote

---
+++ Four.java
@@ -0,0 +1,29 @@
+package day5_array;
+
+public class Four {
+
+	public static void main(String[] args) {
+
+		int arr[]= {2,6,3,8,9,0,1,10};
+		
+		String array[] = {"pooja", "Prasad", "aman", "khushi", "muskan"};
+		
+		
+		//for each loop / 
+		for(int a : arr) {
+			System.out.print(a+" ");
+		}
+		
+		System.out.println();
+		
+
+		for(String b : array) {
+			System.out.print(b+" ");
+		}
+			
+		
+		
+		
+	}
+
+}
